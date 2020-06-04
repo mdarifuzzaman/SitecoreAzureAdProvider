3850b7a495e125135686d16af6acd993a5b3403c
Md Arifuzzaman <arif_uap@outlook.com>
2020-06-04 10:44:58 +1000

    first Commit

---
+++ Helpers/UserPropertyInitializer.cs
@@ -0,0 +1,41 @@
+﻿using Sitecore;
+using Sitecore.Diagnostics;
+using Sitecore.Owin.Authentication.Collections;
+using Sitecore.Owin.Authentication.Services;
+using Sitecore.Security.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureAdProviderApp.Helpers
+{
+    public class UserPropertyInitializer: PropertyInitializer
+    {
+        private const string AzureAdClaimName = "AzureAd";
+        protected override void MapCore(User user, ClaimCollection claimCollection)
+        {
+            Assert.ArgumentNotNull((object)user, nameof(user));
+            Assert.ArgumentNotNull((object)claimCollection, nameof(claimCollection));
+
+            base.MapCore(user, claimCollection);
+
+            var isAdminGroup = claimCollection.Any(e => e.Value.Contains("2ea20d7b-4cd3-4a36-b760-39208ef50290"));
+            var azureClaim = claimCollection.Single(e => e.Type == "idp");
+            var isAzureAd = azureClaim.Value == AzureAdClaimName;
+
+            if (!user.RuntimeSettings.IsVirtual && (isAdminGroup && isAzureAd))
+            {
+                if(MainUtil.GetBool(user.Profile.GetCustomProperty("IsAdminMapped"), false))
+                {
+                    user.Profile.RemoveCustomProperty("IsAdminMapped");
+                }
+
+                user.Profile.IsAdministrator = true;
+                user.Profile.SetCustomProperty("IsAdminMapped", "True");
+            }
+                
+            user.Profile.Save();
+        }
+    }
+}
---
+++ Pipelines/AzureAd/AzureADIdentityProviderProcessor.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.IdentityModel.Logging;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using Sitecore.Abstractions;
+using Sitecore.Diagnostics;
+using Sitecore.Owin.Authentication.Configuration;
+using Sitecore.Owin.Authentication.Pipelines.IdentityProviders;
+using Sitecore.Owin.Authentication.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AzureAdProviderApp.Pipelines.AzureAd
+{
+    public class AzureADIdentityProviderProcessor : IdentityProvidersProcessor
+    {
+        public AzureADIdentityProviderProcessor(FederatedAuthenticationConfiguration federatedAuthenticationConfiguration, Microsoft.Owin.Infrastructure.ICookieManager cookieManager, BaseSettings settings) : 
+            base(federatedAuthenticationConfiguration, cookieManager, settings)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            IdentityModelEventSource.ShowPII = true;
+        }
+
+        protected override string IdentityProviderName => "AzureAd";
+
+        protected override void ProcessCore(IdentityProvidersArgs args)
+        {
+            Assert.ArgumentNotNull(args, nameof(args));
+
+            var identityProvider = this.GetIdentityProvider();
+            var authenticationType = this.GetAuthenticationType();
+
+            string aadInstance = Settings.GetSetting("AADInstance");
+            string tenant = Settings.GetSetting("Tenant");
+            string clientId = Settings.GetSetting("ClientId");
+            string postLogoutRedirectURI = Settings.GetSetting("PostLogoutRedirectURI");
+            string redirectURI = Settings.GetSetting("RedirectURI");
+            string authority = string.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+
+            args.App.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                Caption = identityProvider.Caption,
+                AuthenticationType = authenticationType,
+                AuthenticationMode = AuthenticationMode.Passive,
+                ClientId = clientId,
+                Authority = authority,
+                PostLogoutRedirectUri = postLogoutRedirectURI,
+                RedirectUri = redirectURI,
+                Scope = "offline_access",
+
+                // Watch for Events
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    // When everything is passed
+                    SecurityTokenValidated = async notification =>
+                    {
+                        // Get the Ident object from Ticket
+                        var identity = notification.AuthenticationTicket.Identity;
+
+                        // Use Sitecore Claim Transformation Service to generate additional claims like role or admin
+                        foreach (var claimTransformationService in identityProvider.Transformations)
+                        {
+                            claimTransformationService.Transform(identity,
+                                new TransformationContext(FederatedAuthenticationConfiguration, identityProvider));
+                        }
+
+                        // Create new Auth Ticket
+                        notification.AuthenticationTicket = new AuthenticationTicket(identity, notification.AuthenticationTicket.Properties);
+
+                        //Returns blank task
+                        return;
+                    }
+
+                }
+            });
+        }
+    }
+}
