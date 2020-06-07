6f9906524edc4b3123585e335e11963245cfe25b
Md Arifuzzaman <arif_uap@outlook.com>
2020-06-07 11:02:35 +1000

    Add okta login as well

--- Helpers/UserPropertyInitializer.cs
+++ Helpers/UserPropertyInitializer.cs
@@ -13,6 +13,7 @@ namespace AzureAdProviderApp.Helpers
     public class UserPropertyInitializer: PropertyInitializer
     {
         private const string AzureAdClaimName = "AzureAd";
+        private const string OktaClaimName = "Okta";
         protected override void MapCore(User user, ClaimCollection claimCollection)
         {
             Assert.ArgumentNotNull((object)user, nameof(user));
@@ -21,19 +22,38 @@ namespace AzureAdProviderApp.Helpers
             base.MapCore(user, claimCollection);
 
             var isAdminGroup = claimCollection.Any(e => e.Value.Contains("2ea20d7b-4cd3-4a36-b760-39208ef50290"));
-            var azureClaim = claimCollection.Single(e => e.Type == "idp");
-            var isAzureAd = azureClaim.Value == AzureAdClaimName;
+            var isOktaAdminGroup = claimCollection.Any(e => e.Value.Contains("OktaAdmins"));
+            var claim = claimCollection.Single(e => e.Type == "idp");
+            var isAzureAd = claim.Value == AzureAdClaimName;
+            var isOkta = claim.Value == OktaClaimName;
 
-            if (!user.RuntimeSettings.IsVirtual && (isAdminGroup && isAzureAd))
+            if (isAzureAd)
             {
-                if(MainUtil.GetBool(user.Profile.GetCustomProperty("IsAdminMapped"), false))
+                if (!user.RuntimeSettings.IsVirtual && (isAdminGroup))
                 {
-                    user.Profile.RemoveCustomProperty("IsAdminMapped");
-                }
+                    if (MainUtil.GetBool(user.Profile.GetCustomProperty("IsAdminMapped"), false))
+                    {
+                        user.Profile.RemoveCustomProperty("IsAdminMapped");
+                    }
 
-                user.Profile.IsAdministrator = true;
-                user.Profile.SetCustomProperty("IsAdminMapped", "True");
+                    user.Profile.IsAdministrator = true;
+                    user.Profile.SetCustomProperty("IsAdminMapped", "True");
+                }
             }
+            else if (isOkta)
+            {
+                if (!user.RuntimeSettings.IsVirtual && (isOktaAdminGroup))
+                {
+                    if (MainUtil.GetBool(user.Profile.GetCustomProperty("IsAdminMapped"), false))
+                    {
+                        user.Profile.RemoveCustomProperty("IsAdminMapped");
+                    }
+
+                    user.Profile.IsAdministrator = true;
+                    user.Profile.SetCustomProperty("IsAdminMapped", "True");
+                }
+            }
+            
                 
             user.Profile.Save();
         }
---
+++ Pipelines/Okta/OktaIdentityProviderProcessor.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Okta.AspNet;
+using Owin;
+using Sitecore.Abstractions;
+using Sitecore.Diagnostics;
+using Sitecore.Owin.Authentication.Configuration;
+using Sitecore.Owin.Authentication.Pipelines.IdentityProviders;
+using Sitecore.Owin.Authentication.Services;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+
+namespace AzureAdProviderApp.Pipelines.Okta
+{
+    public class OktaIdentityProviderProcessor : IdentityProvidersProcessor
+    {
+        public OktaIdentityProviderProcessor(FederatedAuthenticationConfiguration federatedAuthenticationConfiguration, Microsoft.Owin.Infrastructure.ICookieManager cookieManager, BaseSettings settings) :
+            base(federatedAuthenticationConfiguration, cookieManager, settings)
+        {
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+        }
+
+        protected override string IdentityProviderName => "Okta";
+
+        protected override void ProcessCore(IdentityProvidersArgs args)
+        {
+            Assert.ArgumentNotNull(args, nameof(args));
+
+            var identityProvider = this.GetIdentityProvider();
+            var authenticationType = this.GetAuthenticationType();
+
+            string orgUri = Settings.GetSetting("OrgUri");
+            string clientSecret = Settings.GetSetting("ClientSecret");
+            string clientId = Settings.GetSetting("ClientId");
+            string postLogoutRedirectURI = Settings.GetSetting("PostLogoutRedirectURI");
+            string redirectURI = Settings.GetSetting("RedirectURI");
+
+            args.App.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                Caption = identityProvider.Caption,
+                AuthenticationType = authenticationType,
+                AuthenticationMode = AuthenticationMode.Passive,
+                ClientId = clientId,
+                ClientSecret = clientSecret,
+                Authority = orgUri,
+                RedirectUri = redirectURI,
+                ResponseType = Microsoft.IdentityModel.Protocols.OpenIdConnect.OpenIdConnectResponseType.CodeIdToken,
+                Scope = Microsoft.IdentityModel.Protocols.OpenIdConnect.OpenIdConnectScope.OpenIdProfile,
+                TokenValidationParameters = new TokenValidationParameters { NameClaimType = "name" },
+
+                // Watch for Events
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    // When everything is passed
+                    SecurityTokenValidated = async notification =>
+                    {
+                        // Get the Ident object from Ticket
+                        var identity = notification.AuthenticationTicket.Identity;
+
+                        // Use Sitecore Claim Transformation Service to generate additional claims like role or admin
+                        foreach (var claimTransformationService in identityProvider.Transformations)
+                        {
+                            claimTransformationService.Transform(identity,
+                                new TransformationContext(FederatedAuthenticationConfiguration, identityProvider));
+                        }
+
+                        // Create new Auth Ticket
+                        notification.AuthenticationTicket = new AuthenticationTicket(identity, notification.AuthenticationTicket.Properties);
+
+                        //Returns blank task
+                        return;
+                    },
+                    AuthorizationCodeReceived =  async n =>
+                    {
+                        foreach (var group in n.AuthenticationTicket.Identity.Claims.Where(x => x.Type == "groups"))
+                        {
+                            n.AuthenticationTicket.Identity.AddClaim(new Claim(ClaimTypes.Role, group.Value));
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
